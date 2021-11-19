2441d24778229de8a1128aff7347fc103c7d77d6
Mariana <marianads18@hotmail.com>
2021-11-19 16:37:43 -0300

    primeira-tela-completa

--- mfe-home/projects/mfe-home/src/app/cadastro/cadastro.component.ts
+++ mfe-home/projects/mfe-home/src/app/cadastro/cadastro.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { CadastroService } from './cadastro.service';
 import { DadosCadastrais } from './dados-cadastrais';
@@ -12,57 +12,49 @@ import { DadosCadastrais } from './dados-cadastrais';
 })
 export class CadastroComponent implements OnInit {
 
-  cadastroForm!: FormGroup;
+  cadastroForm: FormGroup;
   clientesCadastrados: DadosCadastrais[] = [];
 
   constructor(
-    private formBuilder: FormBuilder,
     private cadastroService: CadastroService,
     private router: Router,
     private route: ActivatedRoute,
     private http: HttpClient
-  ) { }
-
-  ngOnInit(): void {
-    this.cadastroForm = this.formBuilder.group({
-      nomeCompleto: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
-      email: [null, [Validators.required, Validators.email]],
-      cpf: [null, [Validators.required, Validators.minLength(11), Validators.maxLength(11)]],
-      dataNascimento: [null, [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
-      dataCadastro: [null, [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
-      salarioMensal: [null, [Validators.required]],
-      senha: [null, [Validators.required, Validators.minLength(6)]],
-      endereco: this.formBuilder.group({
-        cep: [null, [Validators.required, CadastroService.cepValidator]],
-        numero: [null, Validators.required],
-        complemento: [null],
-        rua: [null, Validators.required],
-        bairro: [null, Validators.required],
-        cidade: [null, Validators.required],
-        estado: [null, Validators.required],
+  ) {
+    this.cadastroForm = new FormGroup({
+      nomeCompleto: new FormControl (null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
+      email: new FormControl (null, [Validators.required, Validators.email]),
+      cpf: new FormControl (null, [Validators.required, Validators.minLength(11), Validators.maxLength(11)]),
+      dataNascimento: new FormControl (null, [Validators.required, Validators.minLength(10), Validators.maxLength(10)]),
+      dataCadastro: new FormControl (null, [Validators.required, Validators.minLength(10), Validators.maxLength(10)]),
+      salarioMensal: new FormControl (null, [Validators.required]),
+      senha: new FormControl (null, [Validators.required, Validators.minLength(6)]),
+      endereco: new FormGroup({
+        cep: new FormControl (null, [Validators.required, CadastroService.cepValidator]),
+        numero: new FormControl (null, Validators.required),
+        complemento: new FormControl ( null),
+        rua: new FormControl(null, Validators.required),
+        bairro: new FormControl (null, Validators.required),
+        cidade: new FormControl (null, Validators.required),
+        estado: new FormControl (null, Validators.required),
       }),
-      numeroCelular: [null, [Validators.required, Validators.minLength(11), Validators.maxLength(11)]]
+      numeroCelular: new FormControl (null, [Validators.required, Validators.minLength(11), Validators.maxLength(11)])
     });
   }
 
+  ngOnInit(): void {
+
+  }
+
   onSubmit() {
-    /* const cadastro: DadosCadastrais = {}
 
-    this.cadastroService
-      .cadastrarCliente(cadastro)
-      .subscribe((data) => {
-        console.log(data);
-      });
-
-    console.log(cadastro);
   }
 
   consultaCEP(cep: string) {
     this.http.get(`//viacep.com.br/ws/${cep}/json/`)
     .subscribe((dados: any) => console.log(dados))
   }
-   */
-}
+
 }
 
 
